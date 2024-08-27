9c36ff035587db56933029a4e5c43bbc5596fc29
Marcelo Araiza Molina <armamol3@gmail.com>
2024-08-26 18:54:08 -0600

    formulario de compras

--- obj/Release/net8.0/WebApplication2.AssemblyInfo.cs
+++ obj/Release/net8.0/WebApplication2.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6deb16b8959d09456e00a3fefea54dc10fd34665")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
