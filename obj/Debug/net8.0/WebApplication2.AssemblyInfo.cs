41ead3f693cbe22f3f23d2a828f93581ae01aa7f
Marcelo Araiza Molina <armamol3@gmail.com>
2024-11-05 17:59:24 -0600

    cambios en diseño

--- obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54af383e7243ea55c1e7094c1ae7340abb04a68c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67d95f33cf58704f80420ce500c2f1dd64b45767")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
